8530d70182a1ff53d2a156d059baf8d7379db9f2
Jacob <jacobdhartzell@gmail.com>
2026-02-19 12:48:56 -0500

    Initialize

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+// TODO: Define constants for the form and the result display area
+
+// TODO: Add the first line of the event listener to handle form submission
+
+    // Prevent the form from refreshing the page
+    event.preventDefault();
+
+// TODO: Get the number from the form input
+
+// TODO: Write FizzBuzz logic here
+// If divisible by both 3 and 5, set the output to "FizzBuzz"
+// If divisible by 3, set the output to "Fizz"
+// If divisible by 5, set the output to "Buzz"
+// Otherwise, set the output to the number itself
+
+// Display the result on the page
+result.textContent = output;
