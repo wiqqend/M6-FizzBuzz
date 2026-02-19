809f6f71589ddac0cd16be3b694778314574a0c2
Jacob <jacobdhartzell@gmail.com>
2026-02-19 13:01:54 -0500

    event listener and fizzbuzz logic

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,7 +4,6 @@ let submit = document.getElementById('submitBtn');
 let result = document.getElementById('result');
 // TODO: Add the first line of the event listener to handle form submission
 
-    // Prevent the form from refreshing the page
     event.preventDefault();
 
 // TODO: Get the number from the form input
